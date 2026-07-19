e276e039bcad8e65546576b2152aa2effad0797d
Jaan Soulier <jaansoulier@gmail.com>
2026-07-19 18:48:28 -0400

    fix accidental switch to immediate

--- src/main.c
+++ src/main.c
@@ -356,7 +356,7 @@ SDL_AppResult SDLCALL SDL_AppInit(void** appstate, int argc, char** argv)
         SDL_Log("Failed to claim window: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    SDL_SetGPUSwapchainParameters(device, window, SDL_GPU_SWAPCHAINCOMPOSITION_SDR, SDL_GPU_PRESENTMODE_IMMEDIATE);
+    SDL_SetGPUSwapchainParameters(device, window, SDL_GPU_SWAPCHAINCOMPOSITION_SDR, SDL_GPU_PRESENTMODE_MAILBOX);
     color_format = SDL_GetGPUSwapchainTextureFormat(device, window);
     depth_format = SDL_GPU_TEXTUREFORMAT_D32_FLOAT;
     if (!CreateAtlas())
